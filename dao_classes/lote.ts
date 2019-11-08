4c63087ba5a07cfbc672ca8a7b8a078f30326184
trotsdeveloper <trots.developer@protonmail.com>
2019-11-07 19:41:11 -0500

    1 file deleted

---
+++ dao_classes/lote.ts
@@ -0,0 +1,13 @@
+export class Finca {
+  n: string;
+	nombre: string;
+	departamento: string;
+  municipio: string;
+	corregimiento: string;
+	vereda: string;
+	clima: number;
+	altitud: number;
+	temperaturaMedia: number;
+	precipitacionMedia: number;
+	constructor() {}
+}
